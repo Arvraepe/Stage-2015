010b1441c3088637fb9cd06b26239038be315d8a
GlennHue <glenn.vanlooveren@student.kdg.be>
2015-04-01 16:34:40 +0200

    bugfix makeUserResult

--- server/factory/userFactory.js
+++ server/factory/userFactory.js
@@ -2,23 +2,26 @@
  * Created by Glenn on 1-4-2015.
  */
 exports.makeUserResult = function makeUserResult(username, email, _id, firstname, lastname, cb) {
-    var result =
-    {
-        'user' :
-        {
-            'username' : username,
-            'email' : email,
-            '_id' : _id,
-            'firstname' : firstname,
-            'lastname' : lastname
-        }
-    };
-    cb(result);
+    makeUserResultsPrivate(username, email, _id, firstname, lastname, cb);
 };
 
 exports.makeUserLoginResult = function(username, email, _id, firstname, lastname, cb) {
-    makeUserResult(username, email, _id, firstname, lastname, function (result) {
+    makeUserResultsPrivate(username, email, _id, firstname, lastname,  function (result) {
         result.sessionid = 5; //todo actually manage sessionids
         result.user.role = 'user';
     })
 };
+
+function makeUserResultsPrivate(username, email, _id, firstname, lastname, cb) {
+    var result =
+    {
+        'user': {
+            'username': username,
+            'email': email,
+            '_id': _id,
+            'firstname': firstname,
+            'lastname': lastname
+        }
+    };
+    cb(result);
+}
