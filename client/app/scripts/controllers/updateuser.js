4cfe8e9330975e61d2174629f91a17e5d2ed3fe3
sam <sam.baeck@student.kdg.be>
2015-04-10 14:29:39 +0200

    updateUser has been changed

--- client/app/scripts/controllers/updateuser.js
+++ client/app/scripts/controllers/updateuser.js
@@ -9,19 +9,40 @@
  */
 angular.module('stageprojectApp')
   .controller('UpdateuserCtrl', ['$scope', 'userFactory', 'Session', '$window', function ($scope, userFactory, Session, $window) {
-    $scope.userinfotochange = {
-      firstname: '',
-      lastname: ''
-    };
+    $scope.userinfotochange = $scope.currentUser;
+    $scope.confirmpassword= '';
     var sId = Session.getId();
 
+    /*if($scope.updateUserForm.oldpasswordField.$dirty){
+      $scope.updateUserForm.newpasswordField.$setRequired();
+      $scope.updateUserForm.confirmPasswordField;
+    }*/
+    $scope.copyOfUserInfoToChange = {};
+
+
     $scope.updateUser = function (userinfotochange) {
-      userFactory.updateUser(userinfotochange, function (data) {
-        $scope.setCurrentUser(data.user, sId);
-        $window.location.href = '#/dashboard';
-      }, function () {
-        console.log('updating user failed');
-      });
+      if($scope.passwordForm.$invalid && $scope.updateUserForm.firstnameField.$valid &&
+        $scope.updateUserForm.lastnameField.$valid && $scope.updateUserForm.emailField.$valid){
+        $scope.copyOfUserInfoToChange.firstname = userinfotochange.firstname;
+        $scope.copyOfUserInfoToChange.lastname = userinfotochange.lastname;
+        $scope.copyOfUserInfoToChange.email = userinfotochange.email;
+        userFactory.updateUser($scope.copyOfUserInfoToChange, function(data){
+          $scope.setCurrentUser(data.user, sId);
+          $window.location.href = '#/dashboard';
+        });
+      }
+      else if($scope.passwordForm.$valid && $scope.updateUserForm.firstnameField.$valid &&
+        $scope.updateUserForm.lastnameField.$valid && $scope.updateUserForm.emailField.$valid){
+        $scope.copyOfUserInfoToChange.firstname = userinfotochange.firstname;
+        $scope.copyOfUserInfoToChange.lastname = userinfotochange.lastname;
+        $scope.copyOfUserInfoToChange.email = userinfotochange.email;
+        $scope.copyOfUserInfoToChange.oldPassword = userinfotochange.oldPassword;
+        $scope.copyOfUserInfoToChange.newPassword = userinfotochange.newPassword;
+        userFactory.updateUser($scope.copyOfUserInfoToChange, function (data) {
+          $scope.setCurrentUser(data.user, sId);
+          $window.location.href = '#/dashboard';
+        });
+      }
     }
 
 
